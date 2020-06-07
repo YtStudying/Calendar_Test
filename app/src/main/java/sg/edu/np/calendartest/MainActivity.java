d2d0c0186edcb7c1ffbf9383ac613751f23caf47
Hou Ming Loke <s10195290@connect.np.edu.sg>
2020-06-08 05:56:51 +0800

    added view password feature for login and minor adjustments to the design

--- app/src/main/java/sg/edu/np/calendartest/MainActivity.java
+++ app/src/main/java/sg/edu/np/calendartest/MainActivity.java
@@ -4,12 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.widget.ToggleButton;
 
 import java.util.Calendar;
 
@@ -17,7 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText emailId, password;
     Button bt_login;
-    TextView noAcc, forgetPass;
+    ToggleButton bt_view;
+    TextView noAcc;
     DatabaseHelper db;
 
     CustomCalendarView customCalendarView;
@@ -28,11 +34,22 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
         db = new DatabaseHelper(this);
+        bt_view = (ToggleButton)findViewById(R.id.btn_view);
         emailId = (EditText)findViewById(R.id.username);
         password = (EditText)findViewById(R.id.password);
         bt_login = (Button)findViewById(R.id.btn_login);
         noAcc = (TextView)findViewById(R.id.noAcc);
-        forgetPass = (TextView)findViewById(R.id.forgot_password);
+
+        bt_view.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if (isChecked) {
+                    password.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+                } else {
+                    password.setTransformationMethod(PasswordTransformationMethod.getInstance());
+                }
+            }
+        });
+
         noAcc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
