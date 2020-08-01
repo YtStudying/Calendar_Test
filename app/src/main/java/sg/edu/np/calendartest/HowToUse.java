0ad13dcd7cb2bc27927c869332a87dd897fb15e8
james <JamesYuRenxiang>
2020-08-02 06:59:11 +0800

    Test

--- app/src/main/java/sg/edu/np/calendartest/HowToUse.java
+++ app/src/main/java/sg/edu/np/calendartest/HowToUse.java
@@ -2,7 +2,10 @@ package sg.edu.np.calendartest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HowToUse extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class HowToUse extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_how_to_use);
+
+        Button Exit;
+
+        Exit = findViewById(R.id.back);
+
+        Exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Info.class));
+            }
+        });
     }
 }
